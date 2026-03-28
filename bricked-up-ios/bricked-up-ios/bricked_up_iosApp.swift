5a900980e2c6ac396ca1d2cfd6779f8489f58f42
Saumya Jain <samjain0913@gmail.com>
2026-03-28 17:54:57 -0400

    init ios

---
+++ bricked-up-ios/bricked-up-ios/bricked_up_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bricked_up_iosApp.swift
+//  bricked-up-ios
+//
+//  Created by Saumya Jain on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct bricked_up_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
