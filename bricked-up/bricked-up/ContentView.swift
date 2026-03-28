ba035956b79c733f63b9f831d66527674c3f1256
Saumya Jain <samjain0913@gmail.com>
2026-03-28 17:37:32 -0400

    plan

---
+++ bricked-up/bricked-up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bricked-up
+//
+//  Created by Saumya Jain on 3/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bricked-up/bricked-up/bricked_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  bricked_upApp.swift
+//  bricked-up
+//
+//  Created by Saumya Jain on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct bricked_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
